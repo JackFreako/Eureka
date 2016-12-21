a6570fd145f46fba9dce115f1ab6b0fa1ef8b8a5
anhkuong <jskhuong@gmail.com>
2016-12-20 22:07:21 -0600

    add ProductController

---
+++ Eureka/src/main/java/edu/mum/eureka/dao/ProductRepositoryLite.java
@@ -0,0 +1,22 @@
+package edu.mum.eureka.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import edu.mum.eureka.domain.Product;
+
+ 
+
+public interface ProductRepositoryLite {
+
+	List <Product> getAllProducts();
+	
+	Product getProductById(String key);
+	
+	List<Product> getProductsByCategory(String category);
+
+	Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
+	
+	void addProduct(Product product);		
+}
